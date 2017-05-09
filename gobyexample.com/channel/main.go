f4ae8f30999262c69b6c900075f5e4f8014e4d7c
harveyz <zharvey@163.com>
2017-05-10 01:24:05 +0800

    channel directions

--- gobyexample.com/channel/main.go
+++ gobyexample.com/channel/main.go
@@ -2,7 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+// 双向channel可以转换为单向channel, 作为函数参数要做严格限制
+func worker(c chan<- bool) {
+	fmt.Println("worker working")
+	time.Sleep(time.Second * 3)
+	c <- true
+}
+
+func ping(pings chan<- string, s string) {
+	pings <- s
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	pongs <- (<-pings)
+}
 
 func main() {
 	ch := make(chan string)
@@ -18,4 +34,14 @@ func main() {
 	fmt.Println("chan: ", <-buf_chan)
 	buf_chan <- "message"
 	fmt.Println("chan: ", <-buf_chan)
+
+	wc := make(chan bool, 1)
+	go worker(wc)
+	fmt.Println(<-wc)
+	fmt.Println("worker done")
+
+	pings, pongs := make(chan string, 1), make(chan string, 1)
+	ping(pings, "ping...")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
