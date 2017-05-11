ef4541aa3e9d21fdac6208de69144e1b7b998a95
theharveyz <geekwebme@gmail.com>
2017-05-11 06:17:20 -0700

    worker-pool

--- gobyexample.com/worker-pool/main.go
+++ gobyexample.com/worker-pool/main.go
@@ -1,8 +1,16 @@
 package main
 
+func 
+
 func main() {
 	/**
 	 * 官网例子里不是很形象, 这里举个分片计算的例子
 	 */
 	ss := [][]int{[]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9}}
+
+	jobs := make(chain []int, 100)
+	results := make(chan int, 100)
+
+
+
 }
