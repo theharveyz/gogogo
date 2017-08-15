02a2d665bcdfa491c81264128fac506a4b8cb8e4
zhanghuawei7 <zhanghuawei@jd.com>
2017-08-15 11:09:45 +0800

    return nil trap

---
+++ traps/return-nil/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Si interface {
+	Demo() int
+}
+type St struct{}
+
+func (this *St) Demo() int {
+	return 0
+}
+
+func test() *St {
+	return nil
+}
+
+func testReturnInterface() Si {
+	return nil
+}
+
+func main() {
+	a := test()
+	fmt.Println("a is nil", a == nil)
+
+	b := testReturnInterface()
+	fmt.Println("b is nil", b == nil)
+
+	var c Si
+	c = test()
+	fmt.Println("c is nil", c == nil) // 返回false， 即：有类型的nil不能，转换为interface类型时，interface的_data指针不为零值！！
+}
