186f4cea8d9fe0379642b02a912be7dce8a75dc0
HarveyZ <geekwebme@gmail.com>
2017-09-19 15:24:35 +0800

    update sync.WaitGroup test: some wg.Wait

--- sync/wait-group/wait_group_test.go
+++ sync/wait-group/wait_group_test.go
@@ -23,6 +23,13 @@ func TestWaitGroup(t *testing.T) {
 		}(i)
 	}
 
+	go func() {
+		wg.Wait() // 多个Wait 将依次收到通知
+		fmt.Println("我也收到了通知了")
+	}()
 	wg.Wait() //
+	fmt.Println("我收到了通知了")
+	// 我也收到了通知了
+	// wait group exit
 	fmt.Println("wait group exit")
 }
