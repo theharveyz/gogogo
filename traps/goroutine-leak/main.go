2b429f9e2f4f571d3ea73a1c6d0de56f7915e597
harveyz <zharvey@163.com>
2017-09-24 22:41:37 +0800

    add goroutine leak

--- traps/goroutine-leak/main.go
+++ traps/goroutine-leak/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
 	"runtime"
 	"time"
 )
+
+var (
+	PORT string
+)
+
+func init() {
+	PORT = *flag.String("port", "6060", "pprof port")
+}
+func prof() {
+	fmt.Println("pprof http port:", PORT)
+	fmt.Println(http.ListenAndServe("localhost:"+string(PORT), nil))
+}
 
 func main() {
 	ch := make(chan int, 1)
@@ -16,7 +32,7 @@ func main() {
 	}()
 
 	// goroutine leak 资源泄露:
-
+	go prof()
 	// for循环
 	for {
 		time.Sleep(time.Second)
